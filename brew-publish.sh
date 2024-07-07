d18c9dd5327274ae830c2b5244d8a80826f4b6da
Adriano <jld.adriano@gmail.com>
2024-07-06 21:25:06 -0700

    Auto update the nested repo contents

--- brew-publish.sh
+++ brew-publish.sh
@@ -18,3 +18,7 @@ shasum -a 256 dist.generated/$tar_file > dist.generated/$tar_file.sha256
 
 version=$(age-env --version | cut -d' ' -f2)
 age-env run-with-env gh -- gh release create "v$version" --generate-notes dist.generated/$tar_file dist.generated/$tar_file.sha256
+
+sha256=$(cat dist.generated/$tar_file.sha256 | awk '{print $1}')
+sed -i -e "s|sha256 \".*\"|sha256 \"$sha256\"|g" ./homebrew-age-env/Formula/age-env.rb
+sed -i -e "s|version \".*\"|version \"$version\"|g" ./homebrew-age-env/Formula/age-env.rb
