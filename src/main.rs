38daec16aac08582c31b47e370b28525d6535b2d
Adriano <jld.adriano@gmail.com>
2024-07-06 18:07:12 -0700

    Think is kind of working but last test isn't passing

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
  * @description A tool for managing encrypted environments for the age encryption tool
 **/
 use clap::Parser;
+use dotenv_parser;
 use std::env;
 use std::fs;
 use std::fs::File;
@@ -16,26 +17,37 @@ struct Args {
     /// Path to env storage directory
     #[arg(short = 'd', long, default_value_t = String::new())]
     config_dir: String,
-    /// Path to a recipients file
-    #[arg(short, long)]
-    recipients_file: Option<String>,
     #[command(subcommand)]
     command: Command,
 }
 
 #[derive(Parser, Debug)]
 enum Command {
+    Init,
+    List,
     Create {
         /// Name of the environment to create
         name: String,
-        #[arg(short, long)]
+        #[arg(short = 'e', long)]
         from_env_file: Option<String>,
+        #[arg(short = 'r', long)]
+        recipient: Option<String>,
+        #[arg(short = 'R', long)]
+        recipients_file: Option<String>,
+        #[arg(short = 's', long)]
+        from_stdin: bool,
     },
     Delete {
         /// Name of the environment to delete
         name: String,
     },
     DeleteAll,
+    RunWithEnv {
+        /// Name of the environment to run with
+        name: String,
+        #[arg(last = true)]
+        command: Vec<String>,
+    },
 }
 
 fn main() {
@@ -52,42 +64,115 @@ fn main() {
             fs::create_dir(&dir).unwrap();
         }
     }
+    let dir = Path::new(&dir);
+
+    let envs_dir = dir.join("envs");
+    if !envs_dir.exists() {
+        fs::create_dir(&envs_dir).unwrap();
+    }
+    let identities_file = dir.join("identities");
+    if !identities_file.exists() && !matches!(args.command, Command::Init) {
+        panic!(
+            "Identities file {:?} does not exist. Run `age-env init` to create it.",
+            identities_file
+        );
+    }
+
     match args.command {
+        Command::Init => {
+            if identities_file.exists() {
+                panic!(
+                    "Entities file {:?} already exists no need to init",
+                    identities_file
+                );
+            }
+            let mut identities_file = File::create(&identities_file).unwrap();
+
+            let mut identities = String::new();
+            std::io::stdin().read_to_string(&mut identities).unwrap();
+            identities_file.write_all(identities.as_bytes()).unwrap();
+        }
+        Command::List => {
+            let files = fs::read_dir(&envs_dir).unwrap();
+            for file in files {
+                println!("{:?}", file.unwrap().path());
+            }
+        }
         Command::Create {
             name,
             from_env_file,
+            from_stdin,
+            recipient,
+            recipients_file,
         } => {
-            let file = Path::new(&dir).join(name.clone());
+            if recipient.is_some() && recipients_file.is_some() {
+                panic!("Cannot use both --recipient and --recipients-file");
+            }
+
+            let file_path = envs_dir.join(name.clone());
             let env_file = from_env_file.map(|file| Path::new(&dir).join(file));
 
-            if env_file.is_some() {
-                let env_file_path = env_file.as_ref().unwrap();
-                let env_file_exists = env_file_path.exists();
-                if !env_file_exists {
-                    println!(
-                        "Environment file {} does not exist",
-                        env_file_path.display()
+            if from_stdin && env_file.is_some() {
+                panic!("Cannot use both --from-stdin and --from-env-file");
+            }
+
+            if file_path.exists() {
+                panic!("Environment {:?} already exists", file_path);
+            }
+
+            let mut age_command = std::process::Command::new("age");
+
+            match (recipient, recipients_file) {
+                (Some(recipient), None) => {
+                    age_command.arg("-r").arg(&recipient);
+                }
+                (None, Some(recipients_file)) => {
+                    age_command.arg("-R").arg(&recipients_file);
+                }
+                _ => {
+                    panic!(
+                        "Either --recipient or --recipients-file must be provided, but not both"
                     );
                 }
             }
 
-            if !file.exists() {
-                File::create(&file).unwrap();
-                println!("Created environment {} in {:?}", name, file);
-                let mut file = File::open(&file).unwrap();
-                if let Some(env_file) = env_file {
-                    let mut env_file = File::open(&env_file).unwrap();
-                    let mut contents = String::new();
-                    env_file.read_to_string(&mut contents).unwrap();
-                    file.write_all(contents.as_bytes()).unwrap();
-                    println!("Copied environment {:?} to {:?}", env_file, file);
+            age_command.arg("-o").arg(&file_path);
+
+            // Read the environment contents from either mode
+            let env_contents = match from_stdin {
+                true => {
+                    let mut stdin = String::new();
+                    std::io::stdin().read_line(&mut stdin).unwrap();
+                    stdin
                 }
-            } else {
-                panic!("Environment {:?} already exists", file);
+                false => match env_file {
+                    Some(file) => {
+                        let mut file = File::open(&file).unwrap();
+                        let mut contents = String::new();
+                        file.read_to_string(&mut contents).unwrap();
+                        contents
+                    }
+                    None => panic!("No environment file or stdin provided. one of --from-env-file or --from-stdin is required"),
+                }
+            };
+
+            let mut file = File::create(&file_path).unwrap();
+            file.write_all(env_contents.as_bytes()).unwrap();
+
+            let mut child = age_command
+                .stdin(std::process::Stdio::piped())
+                .spawn()
+                .unwrap();
+            {
+                let stdin = child.stdin.as_mut().unwrap();
+                stdin.write_all(env_contents.as_bytes()).unwrap();
             }
+            child.wait().unwrap();
+
+            println!("Created environment {} in {:?}", name, file_path);
         }
         Command::Delete { name } => {
-            let file = Path::new(&dir).join(name.clone());
+            let file = envs_dir.join(name.clone());
             if file.exists() {
                 fs::remove_file(&file).unwrap();
                 println!("Deleted environment {:?}", file);
@@ -124,6 +209,41 @@ fn main() {
                 panic!("Aborted");
             }
             println!("Deleted all environments in {:?}", dir);
+        }
+        Command::RunWithEnv { name, command } => {
+            let file = envs_dir.join(name.clone());
+            if !file.exists() {
+                panic!("Environment {:?} does not exist", file);
+            }
+            let file_contents = fs::read(&file).unwrap();
+            let mut child = std::process::Command::new("age")
+                .arg("-d")
+                .arg("--identity")
+                .arg(&identities_file)
+                .stdin(std::process::Stdio::piped())
+                .stdout(std::process::Stdio::piped())
+                .spawn()
+                .unwrap();
+            {
+                let stdin = child.stdin.as_mut().unwrap();
+                stdin.write_all(&file_contents).unwrap();
+            }
+            child.wait().unwrap();
+            let mut contents = Vec::new();
+            child.stdout.unwrap().read_to_end(&mut contents).unwrap();
+            println!("contents: {:?}", contents);
+            let source = &String::from_utf8(contents).unwrap();
+            let parsed_env = dotenv_parser::parse_dotenv(source).unwrap();
+
+            let mut command_process = std::process::Command::new(command[0].clone());
+
+            for (key, value) in parsed_env.iter() {
+                command_process.env(key, value);
+            }
+            command_process.args(&command[1..]);
+
+            let mut child = command_process.spawn().unwrap();
+            child.wait().unwrap();
         }
     }
 }
---
+++ test.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env sh
+set -eo pipefail
+
+rm -rf test-output.generated
+mkdir test-output.generated
+cd test-output.generated
+
+alias run="cargo run -q -- --config-dir=."
+
+echo "----------------"
+echo "init"
+
+age-keygen > test-key-1.age
+cat test-key-1.age | run init
+export PUBLIC_KEY_1=$(cat test-key-1.age | grep "public key" | cut -d ":" -f 2 | tr -d " ")
+
+age-keygen > test-key-2.age
+cat test-key-2.age | run init
+export PUBLIC_KEY_2=$(cat test-key-2.age | grep "public key" | cut -d ":" -f 2 | tr -d " ")
+
+echo "----------------"
+echo "create"
+echo "TEST=realval" | run create  test-env-1 --from-stdin --recipient $PUBLIC_KEY_1
+
+echo "----------------"
+echo "create"
+echo "TEST=realval" | run create  test-env-2 --from-stdin --recipient $PUBLIC_KEY_2
+
+echo "----------------"
+echo "list"
+run list | grep test-env-1
+run list | grep test-env-2
+
+echo "----------------"
+echo "run-with-env"
+run run-with-env test-env-1 -- zsh -c 'echo "$TEST"' | grep realval
+
+echo "----------------"
+echo "delete"
+run delete test-env-1
+run delete test-env-2
+
+
